1fee14befe733ec6c1354123f461763cd8376466
glshisikdemir <gulsahisikdemir7@gmail.com>
2026-03-27 02:03:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+// frontend dosyalarını sun
 app.use(express.static('.'));
 
+// '/' isteğinde rebuildhub-mvp.html'i gönder
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'rebuildhub-mvp.html'));
+});
+
+// Render için port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
